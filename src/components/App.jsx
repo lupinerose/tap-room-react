175ba8f115edec93f2c8b0ac7814f88e9d6e432d
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-04 14:51:41 -0700

    add routers for new components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,10 @@ import { Switch, Route } from 'react-router-dom'
 import Employee from './Employee'
 import Patron from './Patron'
 import Welcome from './Welcome'
+import EditKeg from './EditKeg'
+import NewKeg from './NewKeg'
+import NewOrder from './NewOrder'
+import SearchKegs from './SearchKegs'
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
         <Route exact path='/' component={Welcome} />
         <Route path='/employee' component={Employee} />
         <Route path='/patron' component={Patron} />
+        <Route path='/employee/new-order' component={NewOrder} />
+        <Route path='/employee/new-keg' component={NewKeg} />
+        <Route path='/employee/edit-keg' component={EditKeg} />
+        <Route path='/employee/search' component={SearchKegs} />
       </Switch>
     </div>
   )
--- src/components/EmployeeNav.jsx
+++ src/components/EmployeeNav.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom'
 function EmployeeNav() {
   return(
     <div className="layout">
-      <Link to="/employee/newOrder" className="link">New Order</Link>
-      <Link to="/employee/newKeg" className="link">New Keg</Link>
+      <Link to="/employee/new-order" className="link">New Order</Link>
+      <Link to="/employee/new-keg" className="link">New Keg</Link>
       <Link to="/employee/search" className="link">Search Kegs</Link>
-      <Link to="/employee/editKeg" className="link">Edit Keg</Link>
+      <Link to="/employee/edit-keg" className="link">Edit Keg</Link>
     </div>
   )
 }

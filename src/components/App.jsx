e04e3baa098bb65578892a1f13a97abc3ee0477b
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-04 11:49:18 -0700

    add switch and route to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import logo from '../assets/taproomlogo.jpg'
 import './App.css'
 import { Link } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import Employee from './Employee'
+import Patron from './Patron'
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route exact path='/' component={App} />
+        <Route path='/employee' component={Employee} />
+        <Route path='/patron' component={Patron} />
+      </Switch>
       <header className="App-header">
         <Link to="/employee">Employee</Link>
         <img src={logo} className="App-logo" alt="logo" />
-        <Link to="/customer">Customer</Link>
+        <Link to="/patron">Patron</Link>
       </header>
     </div>
   )

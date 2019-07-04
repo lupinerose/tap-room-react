5035f1609562b165194095e9b666ceb700bfce8f
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-04 11:56:37 -0700

    update App to include only switch, route and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import logo from '../assets/taproomlogo.jpg'
 import './App.css'
-import { Link } from 'react-router-dom'
 import { Switch, Route } from 'react-router-dom'
 import Employee from './Employee'
 import Patron from './Patron'
@@ -15,11 +13,7 @@ function App() {
         <Route path='/employee' component={Employee} />
         <Route path='/patron' component={Patron} />
       </Switch>
-      <header className="App-header">
-        <Link to="/employee">Employee</Link>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Link to="/patron">Patron</Link>
-      </header>
+      <Welcome/>
     </div>
   )
 }

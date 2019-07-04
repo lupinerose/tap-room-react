bd5e2781a7a57a380bbdfc44a3a2d4ae2f4ca2b0
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-04 15:24:39 -0700

    update styling of links in employee nav

--- src/components/EmployeeNav.jsx
+++ src/components/EmployeeNav.jsx
@@ -5,10 +5,10 @@ import './EmployeeNav.css'
 function EmployeeNav() {
   return(
     <div className="stack">
-        <Link to="/employee/new-order" className="center">New Order</Link><br/>
-        <Link to="/employee/new-keg" className="center">New Keg</Link><br/>
-        <Link to="/employee/search" className="center">Search Kegs</Link><br/>
-        <Link to="/employee/edit-keg" className="center">Edit Keg</Link>
+        <Link to="/employee/new-order" className="center link">New Order</Link><br/>
+        <Link to="/employee/new-keg" className="center link">New Keg</Link><br/>
+        <Link to="/employee/search" className="center link">Search Kegs</Link><br/>
+        <Link to="/employee/edit-keg" className="center link">Edit Keg</Link>
     </div>
   )
 }

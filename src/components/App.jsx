e4490985cb6216c95e9b02dcc4cbadfa3e8ae617
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-13 09:43:40 -0700

    re add reote for employee and patron

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import Employee from './Employee'
+import Patron from './Patron'
 import KegControl from './KegControl'
-
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path='/' component={KegControl} />
+        <Route exact path='/employee' component={Employee} />
+        <Route exact path='/patron' component={Patron} />
       </Switch>
     </div>
   )

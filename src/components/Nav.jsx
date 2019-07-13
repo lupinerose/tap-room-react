abac41966c0d034e0c1d6805d37451643b211386
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-13 11:45:27 -0700

    create conditional rendering in Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,10 +5,19 @@ import PropTypes from 'prop-types'
 function Nav(props) {
   return(
     <div className="stack">
-        <h1 className="center red-text link">New Order</h1><br/>
-        <h1 className="center link">New Keg</h1><br/>
-        <h1 className="center link">Search Kegs</h1><br/>
-        <h1 className="center link">Edit Keg</h1>
+      {props.view === 'employee' &&
+        <div>
+          <h1 className="center red-text link">New Order</h1><br/>
+          <h1 className="center link">New Keg</h1><br/>
+          <h1 className="center link">Search Kegs</h1><br/>
+          <h1 className="center link">Edit Keg</h1>
+        </div>
+      }
+      {props.view === 'patron' && 
+        <div>
+          <h2>Sort Kegs our kegs </h2>
+        </div>
+      }
     </div>
   )
 }

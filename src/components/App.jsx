59283e3b921d21dc09da4550958171c9e2010961
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-13 09:31:40 -0700

    add KegControl component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,13 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Employee from './Employee'
-import Patron from './Patron'
-import Welcome from './Welcome'
-import EditKeg from './EditKeg'
-import NewKeg from './NewKeg'
-import NewOrder from './NewOrder'
-import SearchKegs from './SearchKegs'
+import KegControl from './KegControl'
+
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path='/' component={Welcome} />
-        <Route exact path='/employee' component={Employee} />
-        <Route exact path='/patron' component={Patron} />
-        <Route exact path='/employee/new-order' component={NewOrder} />
-        <Route exact path='/employee/new-keg' component={NewKeg} />
-        <Route exact path='/employee/edit-keg' component={EditKeg} />
-        <Route exact path='/employee/search' component={SearchKegs} />
+        <Route exact path='/' component={KegControl} />
       </Switch>
     </div>
   )
---
+++ src/components/KegControl.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+class KegControl extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  var masterKegList = [
+    {
+      name: 'Thunderhead',
+      pricePint: '5',
+      price8oz: '3',
+      brand: 'Pyramid Brewing Co.',
+      type: 'IPA',
+      alcohol: '6.7',
+      remainingPints: '124',
+      seasonal: 'no',
+      locality: 'the Pacific NorthWest',
+      city: 'Seattle',
+      state: ', Washington',
+      country: 'USA'
+    },
+    {
+      name: 'Proletariat Red',
+      pricePint: '5',
+      price8oz: '3',
+      brand: 'Lompoc Brewing Co.',
+      type: 'Red Ale',
+      alcohol: '6',
+      remainingPints: '64',
+      seasonal: 'no',
+      locality: 'Town',
+      city: 'Portland',
+      state: ', Oregon',
+      country: 'USA'
+    },
+    {
+      name: 'Sleighr Winter Ale',
+      pricePint: '6',
+      price8oz: '3',
+      brand: 'Ninkasi Brewing Co.',
+      type: 'Dark Double Alt Ale',
+      alcohol: '7.8',
+      remainingPints: '20',
+      seasonal: 'yes',
+      locality: 'State',
+      city: 'Eugene',
+      state: ', Oregon',
+      country: 'USA'
+    },
+    {
+      name: 'Corona Extra',
+      pricePint: '5',
+      price8oz: '3',
+      brand: 'Cervecería Modelo',
+      type: 'Pale Lager',
+      alcohol: '4.5',
+      remainingPints: '9',
+      seasonal: 'no',
+      locality: 'Earth',
+      city: '',
+      state: '',
+      country: 'Mexico'
+    },
+    {
+      name: 'Saigon Beer',
+      pricePint: '7',
+      price8oz: '3',
+      brand: 'Sabeco',
+      type: 'Lager',
+      alcohol: '5',
+      remainingPints: '124',
+      seasonal: 'no',
+      locality: 'Earth',
+      city: 'Ho Chi Minh City',
+      state: '',
+      country: 'Vietnam'
+    },
+    {
+      name: 'Pabst Blue Ribbon',
+      pricePint: '3',
+      price8oz: '1',
+      brand: 'Pabst Brewing Co.',
+      type: 'Lager',
+      alcohol: '4',
+      remainingPints: '53',
+      seasonal: 'no',
+      locality: 'the Midwest',
+      city: 'Milwaukee',
+      state: ', Wisconsin',
+      country: 'USA'
+    }
+  ]
+
+  render() {
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+
+}

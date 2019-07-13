89c7bae3305d2d2f8e8dc13e6467ec702dfb00cf
Brooke Kullberg <brookekullberg@gmail.com>
2019-07-13 09:49:32 -0700

    add propTypes and onClick event listener to Welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,17 +2,22 @@ import React from 'react'
 import logo from '../assets/taproomlogo.jpg'
 import './StyleSheet.css'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-function Welcome() {
+function Welcome(props) {
   return (
     <div className="Welcome">
       <header className="Welcome-header">
-        <Link to="/employee" className="link">Employee</Link>
+        <Link to="/employee" onClick={props.onEmployeeOrPatronClick} className="link">Employee</Link>
         <img src={logo} className="Welcome-logo" alt="logo" />
-        <Link to="/patron" className="link">Patron</Link>
+        <Link to="/patron" onClick={props.onEmployeeOrPatronClick} className="link">Patron</Link>
       </header>
     </div>
   )
+}
+
+Welcome.propTypes = {
+  onEmployeeOrPatronClick: PropTypes.func.isRequired
 }
 
 export default Welcome
